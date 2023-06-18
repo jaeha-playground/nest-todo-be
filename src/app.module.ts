2579bec794dccad714b5e0a6d194e978e8e70943
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-18 19:42:42 +0900

    feat: LoggerMiddlewares 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middlewares';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // 모든 경로에 middleware 사용
+  }
+}
