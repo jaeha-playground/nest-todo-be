969ed093b5e1bed010c29e43b3f4a6d804b7fad7
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-19 05:24:38 +0900

    feat: JoinRequestDto 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,28 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { Users } from 'src/entities/Users';
+import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@ApiTags('USER')
+@Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: '내 정보 가져오기' })
   @Get()
-  getUsers() {}
+  getProfile(@User() user: Users) {
+    return user || false;
+  }
 
-  @Post()
-  join() {}
+  @Post('join')
+  async join(@Body() body: JoinRequestDto) {
+    await this.usersService.join({
+      // body.email, body.nickname, body.password
+      email: body.email,
+      nickname: body.nickname,
+      password: body.password,
+    });
+  }
 }
