82d44faaa8a7a0d69ec82b519c06bf573b5940e9
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-19 04:44:50 +0900

    feat: Users 엔티티

---
+++ src/entities/Users.ts
@@ -0,0 +1,70 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Index('email', ['email'], { unique: true })
+@Entity({ schema: 'nest-todo', name: 'users' })
+export class Users {
+  // PK
+  @ApiProperty({
+    example: 1,
+    description: '사용자 아이디',
+  })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  // 이메일
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'test@test.com',
+    description: '이메일',
+    required: true,
+  })
+  @Column('varchar', { name: 'email', unique: true, length: 30 })
+  email: string;
+
+  // 닉네임
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'testnickname',
+    description: '닉네임',
+    required: true,
+  })
+  @Column('varchar', { name: 'nickname', length: 30 })
+  nickname: string;
+
+  // 비밀번호
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'asdf1234',
+    description: '비밀번호',
+    required: true,
+  })
+  @Column('varchar', {
+    name: 'password',
+    unique: true,
+    length: 16,
+    select: false,
+  })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
